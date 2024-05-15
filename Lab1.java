eb6fd3173827ed82608fc1fb149e8812fc4421df
azheasir <email@example.com>
2024-05-15 16:11:45 +0800

    changed 2 files

--- Lab1.java
+++ Lab1.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 //xindezhushi
-//yongyuceshixiugai
+//new new new xiugai last
 public class Lab1 {
     private Map<String, Map<String, Integer>> graph;
 	private Map<String, Integer> node;
